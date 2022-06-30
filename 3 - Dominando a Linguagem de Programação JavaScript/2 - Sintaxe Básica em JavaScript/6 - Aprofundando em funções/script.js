357b8414a929e92f364a76e5f18e0aa2cec73e8c
caionickel <caio.nickel@gmail.com>
2022-06-30 18:33:12 -0300

    Adiciona aula sobre aprofundamento em funções e projeto de calculadora usando javascript

---
+++ 3 - Dominando a Linguagem de Programação JavaScript/2 - Sintaxe Básica em JavaScript/6 - Aprofundando em funções/script.js
@@ -0,0 +1,24 @@
+// tipos de função
+
+// //declarativas
+// function funcao() {
+//     console.log('Mensagem de função declarativa')
+// }
+
+// funcao()
+
+// // expressão de função com nomeação
+// var funcao = function() {
+//     console.log('Sou uma mensagem de função de expressão!')
+// }
+
+// funcao()
+
+
+// Arrow Function
+
+var funcao = () => {
+    console.log('Sou uma arrow function!')
+}
+
+funcao()
